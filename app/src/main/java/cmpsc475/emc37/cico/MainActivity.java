4c02de5187acbd929be16ab2e32f9c626b6dcf33
Eoin Crafferty <EMCrafferty@gmail.com>
2021-03-10 17:31:28 -0500

    init

---
+++ app/src/main/java/cmpsc475/emc37/cico/MainActivity.java
@@ -0,0 +1,31 @@
+package cmpsc475.emc37.cico;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import cmpsc475.emc37.cico.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+    ViewPager viewPager = findViewById(R.id.view_pager);
+    viewPager.setAdapter(sectionsPagerAdapter);
+    TabLayout tabs = findViewById(R.id.tabs);
+    tabs.setupWithViewPager(viewPager);
+    //
+  }
+}
